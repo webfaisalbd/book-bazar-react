7aac51c4a33fbc69bf24ed22dcce2791fbd6a0f4
faisalcsebd <faisal.cse.bd@gmail.com>
2021-09-26 17:17:46 +0600

    three component created like Header,Shop,Product

---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+
+        <div className='title-style'>
+            <h1 className='text-center  fw-bold'>BOOK BAZAR</h1>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand fw-bold" href="/navBar">Home</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="navbar-brand fw-bold" href="/navBar">Category</a>
+                            </li>
+
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-5" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success  fw-bold" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <div className="row">
+                <div className="col-md-9">
+                    eikhane product load
+                </div>
+
+                <div className="col-md-3">
+                    eikhane cart calcualtion
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
